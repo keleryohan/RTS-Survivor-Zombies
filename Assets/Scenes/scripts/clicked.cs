91ba44653ef91fe5477e67476a4171a0ee345a16
Keler Yohan Carvalho Rolim <47726153+keleryohan@users.noreply.github.com>
2020-03-05 07:22:43 -0300

    Add files via upload

---
+++ Assets/Scenes/scripts/clicked.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clicked : MonoBehaviour
+{
+    [SerializeField]
+    private Material selected;
+
+    [SerializeField]
+    private Material unselected;
+
+    private MeshRenderer MRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MRend = GetComponent<MeshRenderer>();
+    }
+
+    public void gotCliked()
+    {
+        MRend.material = selected;
+    }
+}
